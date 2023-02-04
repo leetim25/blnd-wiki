a750a0ec6e29a55a792bf686740c0d60addb5adc
leetim25 <leetim825@gmail.com>
2023-02-03 19:24:45 -0500

    Added more md files

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import MarkDown from 'markdown-to-jsx';
 import './App.css';
 
 function App() {
-  const file_name = "game-mechanic.md"
   const [post, setPost] = useState('');
+  const [fileName, setFileName] = useState('game-mechanic.md');
 
   useEffect(() => {
-    import(`./markdown/${file_name}`)
+    import(`./markdown/${fileName}`)
       .then(res => {
           fetch(res.default)
             .then (res => res.text())
@@ -16,14 +16,32 @@ function App() {
       })
   })
 
+  const clickMenu = (element) => {
+    const menuName = element.target.id;
+    switch (menuName) {
+      case 'game-mechanic':
+        setFileName('game-mechanic.md');
+        break;
+      case 'strategy':
+        setFileName('strategy.md');
+        break;
+      case 'rewards':
+        setFileName('rewards.md');
+        break;
+      case 'patch-notes':
+        setFileName('patch-notes.md');
+    }
+  }
+
   return (
     <div className='wiki'>
       <div className='wiki-header'>Blnd Wiki</div>
       <div className='wiki-wrapper'>
-        <div className='side-menu'>
-            <li>Game Mechnics</li>
-            <li>Strategy</li>
-            <li>Rewards</li>
+        <div className='side-menu' onClick={clickMenu}>
+            <li className="menu-item" id="game-mechanic">Game Mechnics</li>
+            <li className="menu-item" id="strategy">Strategy</li>
+            <li className="menu-item" id="rewards">Rewards</li>
+            <li className="menu-item" id="patch-notes">Patch Notes</li>
         </div>
         <div className='wiki-content'>
           <MarkDown>
--- src/markdown/game-mechanic.md
+++ src/markdown/game-mechanic.md
@@ -1,4 +1,4 @@
-An h1 header
+Game Mechanics
 ============
 
 Paragraphs are separated by a blank line.
